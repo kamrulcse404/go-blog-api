ba1cb9c4a699967e572ef65bb7059da1170714f1
kamrulcse404 <kamrul.h01960@gmail.com>
2026-08-22 06:03:59 +0600

    request time tracking in middleware and some refactor

--- middleware/logger.go
+++ middleware/logger.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type statusWriter struct {
@@ -11,21 +12,32 @@ type statusWriter struct {
 }
 
 func (sw *statusWriter) WriteHeader(code int) {
-	sw.ResponseWriter.WriteHeader(code)
 	sw.statusCode = code
+	sw.ResponseWriter.WriteHeader(code)
+}
+
+func (sw *statusWriter) Write(b []byte) (int, error) {
+	if sw.statusCode == 0 {
+		sw.statusCode = http.StatusOK
+	}
+
+	return sw.ResponseWriter.Write(b)
 }
 
 func Logger(next http.Handler) http.Handler {
 
 	logger := func(w http.ResponseWriter, r *http.Request) {
 
+		start := time.Now()
+
 		sw := &statusWriter{
 			ResponseWriter: w,
-			statusCode: http.StatusOK,
+			statusCode:     0,
 		}
 
 		next.ServeHTTP(sw, r)
-		fmt.Println(r.Method, r.URL.Path, sw.statusCode)
+		duration := time.Since(start)
+		fmt.Println(r.Method, r.URL.Path, sw.statusCode, duration)
 	}
 
 	return http.HandlerFunc(logger)
