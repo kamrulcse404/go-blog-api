7e76a7e16098d362484fa352e19f08064c414017
kamrulcse404 <kamrul.h01960@gmail.com>
2026-08-28 03:48:36 +0600

    connection DB refactoring

--- storage/database.go
+++ storage/database.go
@@ -2,6 +2,10 @@ package storage
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -9,22 +13,32 @@ var DB *sql.DB
 
 func ConnectDB() error {
 
-	var err error
-	connectionString := "host=localhost port=5432 user=bloguser password=blogpass dbname=blogdb sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		return fmt.Errorf("failed to load .env file: %w", err)
+	}
 
-	DB, err = sql.Open(
-		"postgres",
-		connectionString,
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
 	)
 
+	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open database: %w", err)
 	}
 
-	err = DB.Ping()
-	if err != nil {
-		return err
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return fmt.Errorf("failed to ping database: %w", err)
 	}
+
+	DB = db
 
 	return nil
 }
