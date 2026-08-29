9d6fe7c99e7d3b57b20a0c5d8b34b517fda68a13
kamrulcse404 <kamrul.h01960@gmail.com>
2026-08-29 13:41:47 +0600

    user request validation & password hash, post_validation file rename

---
+++ security/password.go
@@ -0,0 +1,18 @@
+package security
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", fmt.Errorf("hash password: %w", err)
+	}
+
+	return string(hash), nil
+}
